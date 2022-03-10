7babb4e668090a0bc4bfa76882a14507842fc1f2
Arjun G Kumar <arjunrapidramp@gmail.com>
2022-03-10 17:25:53 +0530

    mlflowinit

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+#with open("README.md", "r", encoding="utf-8") as f:
+    #long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "MLflow-project-template"
-AUTHOR_USER_NAME = "c17hawke"
+#REPO_NAME = "MLflow_CNN_apps_FSDS"
+#AUTHOR_USER_NAME = "Arju-cmd"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = []
+LIST_OF_REQUIREMENTS = ["numpy"]
 
 
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author=AUTHOR_USER_NAME,
+    #author=AUTHOR_USER_NAME,
     description="A small package for MLflow app",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+   # url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    #author_email="arjungkumar333@@gmail.com",
     packages=[SRC_REPO],
-    license="MIT",
+    #license="MIT",
     python_requires=">=3.6",
     install_requires=LIST_OF_REQUIREMENTS
 )
