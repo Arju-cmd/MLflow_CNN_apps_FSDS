774391e5d3532ff1f6b06595b82bcbe5c8708a32
Arjun G Kumar <arjunrapidramp@gmail.com>
2022-03-13 11:54:31 +0530

    Unzip Ops added

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,4 +1,5 @@
 import os
+from zipfile import ZipFile
 import yaml
 import logging
 import time
@@ -22,3 +23,9 @@ def save_json(path: str, data: dict) -> None:
         json.dump(data, f, indent=4)
 
     logging.info(f"json file saved at: {path}")
+
+def unzip_file(source:str, dest:str) -> None:
+    logging.info(f"Extracted started....")
+    with ZipFile(source, "r") as zip_f :
+        zip_f.extractall(dest)
+    logging.info(f"extracted {source} to {dest}")
